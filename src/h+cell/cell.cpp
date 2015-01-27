5b3b5f3e5bbe08ed83b770ffe2927a9396ef99b6
yann.bouret <yann.bouret@gmail.com>
2015-01-27 10:06:29 +0000

    start modelplos

--- src/h+cell/cell.cpp
+++ src/h+cell/cell.cpp
@@ -13,5 +13,7 @@ lib(L, libID.c_str() ),
 eqs(L, eqsID.c_str(),lib),
 eff(L, effID.c_str() )
 {
+    std::cerr << lib << std::endl;
+    std::cerr << eqs << std::endl;
 }
 
---
+++ src/modelplos/main.cpp
@@ -0,0 +1,42 @@
+#include "../h+cell/cell.hpp"
+
+#include "yocto/lua/lua-state.hpp"
+#include "yocto/lua/lua-config.hpp"
+
+#include "yocto/exception.hpp"
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        // setting up Lua
+        Lua::State VM;
+        lua_State *L = VM();
+        if(argc>1)
+        {
+            Lua::Config::DoFile(L,argv[1]);
+        }
+        for(int i=2;i<argc;++i)
+        {
+            Lua::Config::DoString(L, argv[i]);
+        }
+
+        string libID = "lib";
+        string eqsID = "eqs";
+        string effID = "eff";
+
+        HCell cell(L,libID,eqsID,effID);
+
+        return 0;
+    }
+    catch( const exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << e.when() << std::endl;
+    }
+    catch(...)
+    {
+        std::cerr << "Unhandled exception..." << std::endl;
+    }
+    return 1;
+}
