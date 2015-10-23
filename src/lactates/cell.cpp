3d16a57c1cc8ae42d917d6568b9271910db09cab
ybouret <yann.bouret@gmail.com>
2015-10-23 10:06:04 +0200

    WIP nbclac

---
+++ src/lactates/cell.cpp
@@ -0,0 +1,251 @@
+#include "cell.hpp"
+#include "yocto/exception.hpp"
+#include "yocto/math/core/tao.hpp"
+#include "yocto/lua/lua-config.hpp"
+
+Cell:: ~Cell() throw() {}
+
+Cell:: Cell( lua_State *vm, const double t ) :
+HCell(vm,t,NULL,NULL),
+iNa( lib["Na+"]->indx ),
+iK(  lib["K+"]->indx  ),
+iCl( lib["Cl-"]->indx ),
+iH(  lib["H+"]->indx  ),
+leak_K(  eff["lambda_K"]  ),
+leak_Na( eff["lambda_Na"] ),
+leak_Cl( eff["lambda_Cl"] ),
+NHE( eff["NHE"] ),
+AE2( eff["AE2"] ),
+NaK( eff["NaK"] )
+{
+}
+
+#include "yocto/math/fcn/zfind.hpp"
+
+double Cell:: SteadyStateZeta()
+{
+    tmx = 0.0;
+    in  = inside;
+    ComputeOutsideComposition(tmx);
+
+    
+    const double zeta_step = 0.1;
+    double zeta = 0.1;
+    numeric<double>::function F(this, & Cell:: ComputePassiveZeroFlux);
+    do
+    {
+        const double Fm = F(-zeta);
+        const double Fp = F( zeta);
+        //std::cerr << "zeta=" << zeta << ", Em=" << Z2E * zeta << ", Fm=" << Fm << ", Fp=" << Fp << std::endl;
+        if(Fm*Fp<0)
+        {
+            break;
+        }
+        zeta += zeta_step;
+        if(zeta>ZETA_MAX)
+            throw exception("Cannot find a zero flux potential!");
+    }
+    while(true);
+
+    zfind<double> solve(0);
+    zeta = solve(F,-zeta,zeta);
+    return zeta;
+
+}
+
+
+
+double Cell:: ComputePassiveZeroFlux(double zeta)
+{
+
+    in[iZeta] = zeta;
+
+    double ans = 0;
+    leak_K.rate(rho, tmx, in, out, params);
+    ans += 1.5 * rho[ iK ] * leak_K.pace;
+
+    leak_Na.rate(rho, tmx, in, out, params);
+    ans += rho[ iNa ] * leak_Na.pace;
+
+    leak_Cl.rate(rho, tmx, in, out, params);
+    ans -= rho[ iCl ] * leak_Cl.pace;
+    
+    return ans;
+}
+
+void  Cell:: SetSteadyStatePotential(double Em)
+{
+    in[iZeta] = Em * E2Z;
+    tmx       = 0.0;
+    numeric<double>::function F(this, & Cell:: ComputePassiveZeroKFlux);
+    double factor = 1.1;
+    const double factor_step = 0.1;
+    do
+    {
+        const double Fm = F(1.0/factor);
+        const double Fp = F(factor);
+        if(Fm*Fp<0)
+        {
+            break;
+        }
+        factor += factor_step;
+        if(factor>20)
+            throw exception("Need too much expansion to get Em=%g", Em );
+    }
+    while(true);
+    zfind<double> solve(0);
+    const double pace = solve(F,1.0/factor,factor);
+    std::cerr << "increased by " << pace << std::endl;
+    leak_K.pace = pace;
+
+    inside[iZeta] = in[iZeta];
+}
+
+double Cell:: ComputePassiveZeroKFlux(double pace)
+{
+    leak_K.pace = pace;
+    return ComputePassiveZeroFlux(in[iZeta]);
+}
+
+
+void Cell:: Setup(double Em)
+{
+    ComputeOutsideComposition(0.0);
+    in = inside;
+
+    std::cerr << std::endl << "Setting Up Cell !" << std::endl;
+    std::cerr << eff << std::endl;
+    
+    //__________________________________________________________________________
+    //
+    // Set potential => Lambda etc
+    //__________________________________________________________________________
+    SetSteadyStatePotential(Em);
+
+    //__________________________________________________________________________
+    //
+    // compute the steady state K+ leak
+    //__________________________________________________________________________
+    tao::ld(rho,0);
+    leak_K.rate(rho, tmx, in, out, params);
+    const double lambda_K = rho[iK]*leak_K.pace;
+    std::cerr << "lambda_K=" << lambda_K << std::endl;
+    if(lambda_K>=0)
+    {
+        throw exception("Invalid Potassium Concentrations or Potential!");
+    }
+
+    //__________________________________________________________________________
+    //
+    // compute scaling for NaK
+    //__________________________________________________________________________
+    const double rho_NaK = -lambda_K;
+    std::cerr << "rho_NaK=" << rho_NaK << std::endl;
+    tao::ld(rho,0);
+    NaK.rate(rho, tmx, in, out, params);
+    const double raw_NaK = rho[iK];
+    NaK.pace = rho_NaK/raw_NaK;
+
+
+    //__________________________________________________________________________
+    //
+    // compute scaling for AE2/chloride
+    //__________________________________________________________________________
+    tao::ld(rho,0);
+    leak_Cl.rate(rho, tmx, in, out, params);
+    const double lambda_Cl = rho[iCl]*leak_Cl.pace;
+    if(lambda_Cl>=0)
+    {
+        throw exception("Invalid Chloride Leak: check concentration/potential");
+    }
+    const double rho_AE2 = -lambda_Cl;
+    std::cerr << "rho_AE2=" << rho_AE2 << std::endl;
+    tao::ld(rho,0);
+    AE2.rate(rho, tmx, in, out, params);
+    const double raw_AE2 = rho[iCl];
+    AE2.pace = rho_AE2/raw_AE2;
+
+    //__________________________________________________________________________
+    //
+    // equilibriate for NHE
+    //__________________________________________________________________________
+    const double rho_NHE = rho_AE2;
+    tao::ld(rho,0);
+    NHE.rate(rho, tmx, in, out, params);
+    const double raw_NHE = rho[iNa];
+    NHE.pace = rho_NHE/raw_NHE;
+
+    std::cerr << "Testing..." << std::endl;
+    eff.rate(rho, tmx, in, out, params);
+    for(size_t i=M;i>0;--i)
+    {
+        rho[i] *= (inside[iSurface]*1e-12)/(1e-15*inside[iVolume]);
+    }
+    std::cerr << "rho=" << std::endl;
+    lib.display(std::cerr, rho) << std::endl;;
+
+    eqs.absorb(tmx, rho, in);
+    std::cerr << "rho1=" << std::endl;
+    lib.display(std::cerr, rho) << std::endl;
+
+    std::cerr << eff << std::endl;
+
+}
+
+#include "yocto/sort/quick.hpp"
+
+void Cell:: Rates( array<double> &dYdt, double t, const array<double> &Y )
+{
+    assert(Y.size()>=nvar);
+    assert(dYdt.size()>=nvar);
+
+    //assume Y is normalized ?
+
+    //__________________________________________________________________________
+    //
+    // compute outside
+    //__________________________________________________________________________
+    ComputeOutsideComposition(t);
+    rho.make(M,0);
+    for(size_t i=M;i>0;--i)
+    {
+        rho[i] = Y[i] - out[i];
+    }
+    quicksort(rho);
+    double deltaOsm = 0;
+    for(size_t i=1;i<=M;++i)
+    {
+        deltaOsm += rho[i];
+    }
+
+
+    //__________________________________________________________________________
+    //
+    //evaluate all surface fluxes in moles/s/m^2
+    //__________________________________________________________________________
+    eff.rate(rho, t, Y, out, params);
+
+
+    //__________________________________________________________________________
+    //
+    // convert in dC/dt
+    //__________________________________________________________________________
+    const double S = Y[iSurface] * 1e-12; //!< mu^2
+    const double V = Y[iVolume]  * 1e-15; //!< L
+    const double J2C = S/V;
+    for(size_t i=M;i>0;--i)
+    {
+        dYdt[i] = rho[i]*J2C;
+    }
+    
+    //__________________________________________________________________________
+    //
+    //chemical absorption + constants variation
+    //__________________________________________________________________________
+    eqs.absorb(t, dYdt, Y);
+    
+
+}
+
+
+
---
+++ src/lactates/nbclac.lua
@@ -0,0 +1,341 @@
+-- -----------------------------------------------------------------------------
+--
+-- times
+--
+-- -----------------------------------------------------------------------------
+ftol   = 1e-5; -- differential fractional tolerance
+diff_h = 1e-4; -- initial adaptive time step between to time steps
+
+dt      = 0.2;
+dt_save = 1;
+t_run   = 1500;
+
+T       = 298;
+
+Em0     = -60e-3;
+
+zeta0 = 0 * F / (R*T);
+
+-- -----------------------------------------------------------------------------
+--
+-- standard
+--
+-- -----------------------------------------------------------------------------
+exp  = math.exp;
+tanh = math.tanh;
+
+
+-- -----------------------------------------------------------------------------
+--
+-- Components
+--
+-- -----------------------------------------------------------------------------
+
+lib =
+{
+    {"H+",   1},
+    {"HO-", -1},
+    {"Na+",  1},
+    {"Cl-", -1},
+    {"K+",   1},
+    {"HCO3-",-1},
+    {"CO3--",-2},
+    {"HY",    0},
+    {"Y-",   -1},
+    --{"LacH",  0},
+    --{"Lac-", -1},
+    {"OSM",   0}
+};
+
+-- -----------------------------------------------------------------------------
+-- Thermo
+-- -----------------------------------------------------------------------------
+Kw      = 1.0e-14;
+K_Henry = 29.41;   -- atm/(mol/L)
+
+K1      = 4.45e-7;
+K2      = 5.6e-11;
+
+pKY = 6.2;
+
+function P_CO2(t)
+local  P0    = 40.0;
+local  W     = 300;
+if (t>=5) and (t<=W+5) then
+  return P0/760.0 + (8/760.0) * math.sin(math.pi*(t-5)/W)^2;
+end
+return P0/760;
+end
+
+function kappa(t)
+local  CO2_aq = P_CO2(t) / K_Henry;
+return CO2_aq * K1;
+end
+
+
+-- -----------------------------------------------------------------------------
+--
+-- Equilibria
+--
+-- -----------------------------------------------------------------------------
+pKLac = 3.86
+eqs =
+{
+    { "water", 1e-14, { 1, "H+"}, {  1, "HO-"} },
+    { "bicarb", "kappa",          {  1, "H+" }, { 1, "HCO3-"} },
+    { "carb",   K2,               {  1, "H+" }, { 1, "CO3--"}, { -1, "HCO3-" } },
+    -- { "lactic",10^(-pKLac),       { -1, "LacH"}, {1,"Lac-"},{1,"H+"} },
+    { "buffer", 10^(-pKY),        { -1, "HY" }, { 1, "Y-"   }, {1, "H+" } }
+};
+
+eff =
+{
+    "lambda_Na",
+    "lambda_K",
+    "lambda_Cl",
+    "NaK",
+    "NHE",
+    "AE2"
+    --, "MCT"
+};
+
+
+-- -----------------------------------------------------------------------------
+--
+-- Inside
+--
+-- -----------------------------------------------------------------------------
+NoLactate = { 0, { 1, "LacH"}, {1, "Lac-"}}
+ini =
+{
+    { 10^(-7.2), { 1, "H+" } },
+    { 140e-3,    { 1, "K+"  } },
+    { 10e-3,     { 1, "Na+" } },
+    { 20e-3,     { 1, "Cl-" } },
+    { "osmolarity", 300e-3},
+    -- NoLactate,
+    { 60e-3, { 1, "HY"}, {1, "Y-" } },
+};
+
+
+-- -----------------------------------------------------------------------------
+--
+-- Outside
+--
+-- -----------------------------------------------------------------------------
+
+init0 =
+{
+    { 10^(-7.4),    { 1, "H+"  } },
+    { 4e-3,         { 1, "K+"  } },
+    { 140e-3,       { 1, "Na+" } },
+    { 100e-3,       { 1, "Cl-" } },
+    { "osmolarity", 300e-3 },
+    --NoLactate,
+    { 0, { 1, "HY"}, {1, "Y-" } }
+}
+
+init1 =
+{
+    { 10^(-6.5),    { 1, "H+"  } },
+    { 4e-3,         { 1, "K+"  } },
+    { 140e-3,       { 1, "Na+" } },
+    { 100e-3,       { 1, "Cl-" } },
+    { "osmolarity",  300e-3 },
+    --NoLactate,
+    { 0, { 1, "HY"}, {1, "Y-" } }
+}
+
+
+out =
+{
+    "init0",
+    "init1"
+};
+
+
+function weights(t)
+return 1,0
+end
+
+-- -----------------------------------------------------------------------------
+-- Surface * Permeability in micron^3 / s
+-- -----------------------------------------------------------------------------
+
+function SP_K( t, x )
+return ((3.70714279) * exp( (0.4310085034) * x ) + (1.507274188));
+end
+
+-- -----------------------------------------------------------------------------
+-- Surface * Permeability in micron^3 / s
+-- -----------------------------------------------------------------------------
+function SP_Cl(t,x)
+return (1.194531961) * exp( (0.05658342092) * x );
+end
+
+-- -----------------------------------------------------------------------------
+-- Surface * Permeability in micron^3 / s
+-- -----------------------------------------------------------------------------
+
+function SP_Na(t,x)
+return (1.360058333) * exp( (0.05223441992) * x );
+end
+
+
+-- -----------------------------------------------------------------------------
+-- Article from which the permeabilities were fitted
+-- -----------------------------------------------------------------------------
+
+
+Cm      = 1;   -- muF/cm^2
+
+Capa_exp = 207e-12;               -- from article 207 pF
+Surf_exp = Capa_exp / (1e-14*Cm); -- in micron^2
+
+-- -----------------------------------------------------------------------------
+--
+-- Geometry
+--
+-- -----------------------------------------------------------------------------
+
+
+a = 12.5; -- in microns
+b = 5;    -- in microns
+c = 5;    -- in microns
+
+surface = EllipsoidSurface(a,b,c);
+volume  = EllipsoidVolume(a,b,c);
+
+PermRatio = 30.0/Surf_exp;
+
+-- -----------------------------------------------------------------------------
+-- INWARD Na in  moles/s/m^2
+-- -----------------------------------------------------------------------------
+function lambda_Na(t,Cin,Cout,params)
+local zeta = params["zeta"];
+local zz   = zeta;
+local Na   = "Na+";
+local S    = params["S"];
+local V    = params["V"];
+
+a = {}
+local Perm = SP_Na(t,zeta)*PermRatio;                   -- in microns/s
+local Flux = Perm * Psi(zz)*(Cout[Na]-Cin[Na]*exp(zz)); -- in moles/L*microns/s
+local J    = 1e-3 * Flux;                               -- in moles/m^2/s
+a[Na]      = J;
+return a;
+
+end
+
+-- -----------------------------------------------------------------------------
+-- INWARD chloride moles/s/m^2
+-- -----------------------------------------------------------------------------
+function lambda_Cl(t,Cin,Cout,params)
+local zeta = params["zeta"];
+local zz   = -zeta;
+local Cl = "Cl-";
+
+a = {};
+local Perm = SP_Cl(t,zeta)*PermRatio;                   -- in microns/s
+local Flux = Perm * Psi(zz)*(Cout[Cl]-Cin[Cl]*exp(zz)); -- in moles/L*microns/s
+local J    = 1e-3*Flux;                                 -- in moles/m^2/s
+a[Cl]      = J;
+return a;
+
+end
+
+
+-- -----------------------------------------------------------------------------
+-- INWARD potassium moles/s/m^2
+-- -----------------------------------------------------------------------------
+function lambda_K(t,Cin,Cout,params)
+local zeta = params["zeta"];
+local zz   = zeta;
+local K  = "K+";
+
+a = {};
+local Perm = SP_K(t,zeta)*PermRatio;                   -- in microns/s
+local Flux = Perm * Psi(zz)*(Cout[K]-Cin[K]*exp(zz));  -- in moles/L*microns/s
+local J    = 1e-3*Flux;                                -- in moles/m^2/s
+a[K] = J;
+return a;
+
+end
+
+-- -----------------------------------------------------------------------------
+-- NaK/ATPase INWARD moles/s/m^2, unscaled
+-- -----------------------------------------------------------------------------
+K_NaK = 12e-3;
+
+function NaK(t,Cin,Cout,params)
+local zeta  = params["zeta"];
+local CNa   = Cin["Na+"];
+local sig   = (CNa/(K_NaK+CNa)) * (1+tanh(0.39*zeta+1.28))*0.5;
+local rho   = sig;
+a = {}
+a["K+"]  =  2*rho;
+a["Na+"] = -3*rho;
+return a;
+end
+
+-- -----------------------------------------------------------------------------
+-- NHE INWARD moles/s, unscaled
+-- -----------------------------------------------------------------------------
+
+L0    = 1000;
+Kr    = 1.8e-8;
+Kt    = 3.6e-6;
+KNae  = 31e-3;
+KHout = 2e-7;
+function NHE(t,Cin,Cout,params)
+local C    = Kr/Kt;
+local x    = Cin["H+"]/Kr;
+local xp1  = 1+x;
+local cxp1 = 1+C*x;
+local sig_num = x*xp1 + L0*C*x*cxp1;
+local sig_den = L0*cxp1^2 + xp1^1;
+local sig     = sig_num/sig_den;
+local Nae     = Cout["Na+"];
+local KNaeEff = KNae * (1+Cout["H+"]/KHout);
+local sig_out = Nae / (KNaeEff+Nae);
+sig = sig * sig_out;
+
+ans = {}
+ans["H+"]  = -sig;
+ans["Na+"] =  sig;
+return ans;
+
+end
+
+
+-- -----------------------------------------------------------------------------
+-- AE2 INWARD moles/s
+-- -----------------------------------------------------------------------------
+K_AE = 10e-3;
+function AE2(t,Cin,Cout,params)
+local b   = Cin["HCO3-"];
+local rho = b/(b+K_AE);
+
+a = {}
+a["Cl-"]   = rho;
+a["HCO3-"] = -rho;
+return a;
+
+end
+
+-- -----------------------------------------------------------------------------
+-- MCT
+-- -----------------------------------------------------------------------------
+K_Lac = 30e-3;
+
+function MCT(t,Cin,Cout,params)
+local Lac = Cin["Lac-"];
+
+a = {}
+a["Lac-"] = - Lac/(K_Lac+Lac);
+return a;
+end
+
+
+
+
